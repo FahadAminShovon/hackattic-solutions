7d6b795efd53d21e11e6af2c745cdc38b6a52c86
FahadAminShovon <bm.amin.fahad@gmail.com>
2020-01-08 18:20:41 +0600

    DOWNLOAD problem image

---
+++ reading QR/downloadImage.py
@@ -0,0 +1,20 @@
+import urllib
+import requests,json
+
+def getImageUrl(link):
+    try:
+        PROBLEM_URL =  link
+        getProblem = requests.get(PROBLEM_URL)
+        getProblem = getProblem.json()
+        QR_URL = list(getProblem.keys())[0]
+        QR_URL = getProblem[QR_URL]
+        return QR_URL
+    except :
+        return None
+
+def downloadImage(link):
+    imageUrl = getImageUrl(link)
+    urllib.request.urlretrieve(imageUrl, "img.png")    
+
+if __name__=="__main__":
+    downloadImage("https://hackattic.com/challenges/reading_qr/problem?access_token=84edafdf68f8408b")
