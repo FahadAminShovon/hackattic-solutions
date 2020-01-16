f660def4e2d974b45c68c0ac140a31cbb51cfcc4
FahadAminShovon <bm.amin.fahad@gmail.com>
2020-01-16 14:49:35 +0600

    ADD Restore backup solve

---
+++ Backup restore/README.md
@@ -0,0 +1,20 @@
+solve for porblem [Backup restore](https://hackattic.com/challenges/backup_restore)
+
+install gzip
+```bash
+sudo apt-get install gzip
+```
+
+replace the value of token in solve.py
+
+run solve.py
+
+response [200]
+
+used library base64, gzip, requests, json
+
+**helpful links**
+==
+**gzip** &rarr; [gzip](https://docs.python.org/3/library/gzip.html)
+
+**base64 python decoder** &rarr; [base64](https://docs.python.org/3/library/base64.html)
--- Backup restore/solve.py
+++ Backup restore/solve.py
@@ -1,28 +1,41 @@
-import requests,json,base64
-import sqlalchemy as db
-
-class Database():
-    # replace the user, password, hostname and database according to your configuration according to your information
-    engine = db.create_engine('postgresql://root:1234567890@localhost/backup')
-    def __init__(self):
-        self.connection = self.engine.connect()
-        print("DB Instance created")
-        
+import requests,json,base64,gzip
 
 def getDumpdata(token):
     link = "https://hackattic.com/challenges/backup_restore/problem?access_token="+token
-    response = requests.get(link)
-    response = json.loads(response.content)
-    encodedDump = response["dump"]
-    decodedDump = base64.b64decode(encodedDump,)
-    f = open("backup.backup","wb")
-    f.write(decodedDump)
+    encodedDump = json.loads(requests.get(link).content).get("dump")
+    compressedDump = base64.b64decode(encodedDump)
+    dump = gzip.decompress(compressedDump).decode("utf-8")
+    return dump
+
+def submitAns(token,d):
+    link = "https://hackattic.com/challenges/backup_restore/solve?access_token="+token
+    response = requests.post(link,data=d)
+    print(response.content)
+
+def processDump(dump):
+    # print(dump)
+    userList = dump.splitlines()
+    ansList = []
+    for user in userList:
+        if "alive" in user:
+            tempuser = user.split('\t')
+            ansList.append(tempuser[3])
+
+    return ansList
+
 
 def solve(token):
-    getDumpdata(token)
+    dump = getDumpdata(token)
+    ansList = processDump(dump)
+
+    data = json.dumps({
+        "alive_ssns" : ansList
+    })
+
+    submitAns(token,data)
+
 
 
 if __name__ == "__main__":
     token = "84edafdf68f8408b"
     solve(token)
-    Database()
